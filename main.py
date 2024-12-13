a12d19faa642cc79cb45e58c3688f209b576a6dd
doranl <doranl@educ.college-montreal.qc.ca>
2024-12-13 12:04:00 -0500

    fish

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Liam Doran
+404
+TP5
+"""
+
+import arcade
+
+
+YELLOW = (204, 153, 0)
+
+
+def draw():
+
+    arcade.draw_ellipse_filled(350, 225, 115, 75, YELLOW)
+    arcade.draw_circle_filled(382.5, 240, 5, arcade.color.BLACK)
+
+
+
+
+
+
+
+
+def main():
+    arcade.open_window(700, 450, "art")
+    arcade.set_background_color(arcade.color.BLUEBERRY)
+    arcade.start_render()
+    draw()
+    arcade.finish_render()
+
+    arcade.run()
+
+
+main()
