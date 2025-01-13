4c66fb25065aae0ad5b746a3d6316a185ce794a6
doranl <doranl@educ.college-montreal.qc.ca>
2025-01-13 14:52:34 -0500

    fish

--- main.py
+++ main.py
@@ -7,75 +7,96 @@ TP5
 import arcade
 import random
 
-
 YELLOW = (204, 153, 0)
 BLUE = (175, 199, 237)
 GREEN = (115, 240, 130)
 
-window_width = 700
-window_height = 450
 
-Left_tank = window_width - 600
-Right_tank = window_width - 100
-Bottom_tank = window_height - 350
-Top_tank = window_height - 50
+class FishTank(arcade.Window):
+    def __init__(self):
+        super().__init__(700, 450, "fish")
 
-Leaf_height = Bottom_tank + 120
+        self.window_width = 700
+        self.window_height = 450
 
-rock_point = [
-    (Right_tank - 190, Bottom_tank + 20),
-    (Right_tank - 180, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 170, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 160, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 150, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 140, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 130, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 120, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 110, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 100, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 90, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 80, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 70, Bottom_tank + random.randint(30, 50)),
-    (Right_tank - 30, Bottom_tank + 20),
-]
+        self.left_tank = self.window_width - 600
+        self.right_tank = self.window_width - 100
+        self.bottom_tank = self.window_height - 350
+        self.top_tank = self.window_height - 50
 
-X_Fish = window_width - 175
-Y_Fish = window_height - 125
+        self.leaf_height = self.bottom_tank + 120
 
+        self.rock_point = [
+            (self.right_tank - 190, self.bottom_tank + 20),
+            (self.right_tank - 180, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 170, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 160, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 150, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 140, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 130, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 120, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 110, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 100, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 90, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 80, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 70, self.bottom_tank + random.randint(30, 50)),
+            (self.right_tank - 30, self.bottom_tank + 20),
+        ]
 
-def draw():
+        self.x_fish = self.window_width - 175
+        self.y_fish = self.window_height - 125
 
-    arcade.draw_lrbt_rectangle_filled(Left_tank, Right_tank, Bottom_tank, Top_tank, arcade.color.BLUEBERRY)
-    arcade.draw_lrbt_rectangle_outline(Left_tank, Right_tank, Bottom_tank, Top_tank, arcade.color.BLACK, 5)
+    def on_draw(self):
 
+        arcade.set_background_color(arcade.color.WHITE)
 
+        arcade.draw_lrbt_rectangle_filled(
+            self.left_tank, self.right_tank, self.bottom_tank, self.top_tank, arcade.color.BLUEBERRY
+        )
+        arcade.draw_lrbt_rectangle_outline(
+            self.left_tank, self.right_tank, self.bottom_tank, self.top_tank, arcade.color.BLACK, 5
+        )
 
-    arcade.draw_ellipse_filled(X_Fish, Y_Fish, 75, 45, YELLOW)
-    arcade.draw_circle_filled(X_Fish + 20, Y_Fish + 10, 3.5, arcade.color.BLACK)
-    arcade.draw_triangle_filled(X_Fish, Y_Fish, X_Fish - 50, Y_Fish + 30, X_Fish - 50, Y_Fish - 30, YELLOW)
+        arcade.draw_ellipse_filled(
+            self.x_fish, self.y_fish, 75, 45, YELLOW
+        )
+        arcade.draw_circle_filled(
+            self.x_fish + 20, self.y_fish + 10, 3.5, arcade.color.BLACK
+        )
+        arcade.draw_triangle_filled(
+            self.x_fish, self.y_fish, self.x_fish - 50, self.y_fish + 30, self.x_fish - 50, self.y_fish - 30, YELLOW
+        )
 
-    arcade.draw_circle_filled(X_Fish + 50, Y_Fish + 20, 7, BLUE)
-    arcade.draw_circle_filled(X_Fish + 35, Y_Fish + 35, 7, BLUE)
-    arcade.draw_circle_filled(X_Fish + 50, Y_Fish + 50, 7, BLUE)
+        arcade.draw_text("O", self.x_fish + 40, self.y_fish + 15, BLUE, 20, )
+        arcade.draw_text("O", self.x_fish + 30, self.y_fish + 35, BLUE, 20, )
+        arcade.draw_text("O", self.x_fish + 50, self.y_fish + 50, BLUE, 20, )
 
-    arcade.draw_line(Left_tank + 70, Bottom_tank + 30, Left_tank + 70, Bottom_tank + 170, GREEN, 10)
-    arcade.draw_circle_filled(Left_tank + 70, Bottom_tank + 30, 5, GREEN)
-    arcade.draw_circle_filled(Left_tank + 70, Bottom_tank + 170, 5, GREEN)
-    for i in range (0, 10):
-        arcade.draw_arc_outline(Left_tank + 70, Leaf_height, 100, 100, GREEN, 35, 145, 10)
-        Leaf_height = Leaf_height - 20
+        arcade.draw_line(
+            self.left_tank + 70, self.bottom_tank + 30, self.left_tank + 70, self.bottom_tank + 170, GREEN, 10
+        )
+        arcade.draw_circle_filled(
+            self.left_tank + 70, self.bottom_tank + 30, 5, GREEN
+        )
+        arcade.draw_circle_filled(
+            self.left_tank + 70, self.bottom_tank + 170, 5, GREEN
+        )
+        for i in range(0, 7):
+            arcade.draw_arc_outline(
+                self.left_tank + 70, self.leaf_height, 100, 100, GREEN, 35, 145, 10
+            )
+            self.leaf_height -= 20
+        self.leaf_height += 140
 
+        arcade.draw_polygon_filled(
+            self.rock_point, arcade.color.BLACK
+        )
 
-
-    arcade.draw_polygon_filled(rock_point, arcade.color.BLACK)
+        arcade.draw_text("poisson", self.window_width / 2, 60 , BLUE, 20)
 
 
 def main():
-    arcade.open_window(700, 450, "fish")
-    arcade.set_background_color(arcade.color.WHITE)
-    arcade.start_render()
-    draw()
-    arcade.finish_render()
+    draw = FishTank()
+    # draw.on_draw()
 
     arcade.run()
 
